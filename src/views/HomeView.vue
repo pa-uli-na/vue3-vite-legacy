b211bc7fba272ade6930685c252cccb01fb99178
Paulina Krzyżostan <Paulina.Krzyzostan@rossmann.com.pl>
2022-03-30 13:40:00 +0200

    Dodanie quasar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import TheWelcome from "@/components/TheWelcome.vue";
+import QuasarWelcome from "@/components/QuasarWelcome.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <QuasarWelcome />
+    <!-- <TheWelcome /> -->
   </main>
 </template>
